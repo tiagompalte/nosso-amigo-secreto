373167c82b0c8daf2b6ad052c79d6a326284b7cb
Tiago Mariani Palte <tiagompalte@hotmail.com>
2019-12-08 09:57:57 -0300

    Close #5. Testar o Job Raffle

--- spec/jobs/campaign_raffle_job_spec.rb
+++ spec/jobs/campaign_raffle_job_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe CampaignRaffleJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    ActiveJob::Base.queue_adapter = :test
+    @campaign = create(:campaign, status: :pending)
+    create(:member, campaign: @campaign)
+    create(:member, campaign: @campaign)
+    create(:member, campaign: @campaign)
+    @campaign.reload
+  end
+
+  describe '#perform_later' do
+    it 'should have enqueued job' do
+      expect { described_class.perform_later(@campaign) }.to have_enqueued_job
+    end
+
+    it 'should have enqueued job only once' do
+      CampaignRaffleJob.perform_later(@campaign)
+      expect(CampaignRaffleJob).to have_been_enqueued.exactly(:once)
+    end
+
+    it 'should have enqueued with campaign' do
+      expect { CampaignRaffleJob.set(queue: 'email').perform_later(@campaign) }
+          .to have_enqueued_job.with(@campaign).on_queue('email')
+    end
+  end
 end
